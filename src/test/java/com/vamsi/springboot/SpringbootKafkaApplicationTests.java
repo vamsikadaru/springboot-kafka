68b114631bcd1256eb1196217811bf3d371278a9
vamsi-git-test <vamsikadaru01@gmail.com>
2024-11-29 17:52:17 +0530

    first commit

---
+++ src/test/java/com/vamsi/springboot/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
