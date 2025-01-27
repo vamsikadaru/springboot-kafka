26980270cf66fdb474ba4402e1563841d941291f
vamsi-git-test <vamsikadaru01@gmail.com>
2025-01-27 12:28:04 +0530

    Created a REST API to send Message

---
+++ src/main/java/com/vamsi/springboot/controller/MessageController.java
@@ -0,0 +1,29 @@
+package com.vamsi.springboot.controller;
+
+
+import com.vamsi.springboot.kafka.KafkaProducer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+	private KafkaProducer kafkaProducer;
+	
+	public MessageController(KafkaProducer kafkaProducer) {
+		super();
+		this.kafkaProducer = kafkaProducer;
+	}
+	
+	// http://localhost:8080/api/v1/kafka/publish?message=hello world
+	@GetMapping("/publish")
+	public ResponseEntity<String> publish(@RequestParam("message") String message){
+		kafkaProducer.sendMessage(message);
+		return ResponseEntity.ok("Message sent to the topic");
+	}
+}
