6e5376a958b97e37ea3f8ad105ce5b9d9d729849
vamsi-git-test <vamsikadaru01@gmail.com>
2025-01-27 12:16:28 +0530

    Created a Kafka Producer

--- src/main/java/com/vamsi/springboot/kafka/KafkaProducer.java
+++ src/main/java/com/vamsi/springboot/kafka/KafkaProducer.java
@@ -1,10 +1,14 @@
 package com.vamsi.springboot.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+	
+	private static final LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 	private KafkaTemplate<String, String> kafkaTemplate;
 
 	public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
@@ -12,5 +16,9 @@ public class KafkaProducer {
 		this.kafkaTemplate = kafkaTemplate;
 	}
 	
+	public void sendMessage(String message) {
+		LOGGER.info(String.format("Message sent %s", message));
+		kafkaTemplate.send("javaguides", message);
+	}
 	
 }
